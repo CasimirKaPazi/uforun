ec38af7414a66248a5cd592460fe8ed1ca3c8783
Casimir <jul.lutz@gmx.net>
2015-11-28 11:16:52 +0100

    Remove dublicate eye offset

--- mods/uforun/init.lua
+++ mods/uforun/init.lua
@@ -4,9 +4,3 @@ local modpath = minetest.get_modpath("uforun")
 
 dofile(modpath.."/player.lua")
 dofile(modpath.."/ufo.lua")
-
--- Proberties
-
-minetest.register_on_joinplayer(function(player)
-	player:set_eye_offset({x=0,y=-10,z=0},{x=0,y=0,z=0})
-end)
